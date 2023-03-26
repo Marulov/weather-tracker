1ced1f1c89d1f46e7398760868e5be7e0ecc4bd8
Marulov <nazmmarul@hotmail.com>
2023-03-26 13:52:00 +0300

    application.yml change.

--- src/main/java/com/marulov/weathertracker/dto/WeatherDto.java
+++ src/main/java/com/marulov/weathertracker/dto/WeatherDto.java
@@ -2,11 +2,14 @@ package com.marulov.weathertracker.dto;
 
 import com.marulov.weathertracker.model.Weather;
 
+import java.time.LocalDateTime;
+
 public record WeatherDto(
         String city,
         String country,
-        Integer temperature) {
+        Integer temperature,
+        LocalDateTime updatedTime) {
     public static WeatherDto convert(Weather from) {
-        return new WeatherDto(from.getCity(), from.getCountry(), from.getTemperature());
+        return new WeatherDto(from.getCity(), from.getCountry(), from.getTemperature(), from.getUpdatedTime());
     }
 }
