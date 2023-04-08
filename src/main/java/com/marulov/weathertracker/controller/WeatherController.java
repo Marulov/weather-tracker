62333f17358dbc5215cc9567df8b94a4cc68eac3
Marulov <nazmmarul@hotmail.com>
2023-04-08 15:39:03 +0300

    Added @Validated in WeatherController class.

--- src/main/java/com/marulov/weathertracker/controller/WeatherController.java
+++ src/main/java/com/marulov/weathertracker/controller/WeatherController.java
@@ -4,6 +4,7 @@ import com.marulov.weathertracker.dto.WeatherDto;
 import com.marulov.weathertracker.service.WeatherService;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/api/weather")
+@Validated
 public class WeatherController {
 
     private final WeatherService weatherService;
