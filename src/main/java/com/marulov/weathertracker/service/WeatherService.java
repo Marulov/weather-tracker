9aba04847b9b4a6dcba05af06977281445e903bd
Marulov <nazmmarul@hotmail.com>
2023-04-09 19:07:18 +0300

    Cache and scheduled added for getWeatherByCity method.

--- src/main/java/com/marulov/weathertracker/service/WeatherService.java
+++ src/main/java/com/marulov/weathertracker/service/WeatherService.java
@@ -6,7 +6,14 @@ import com.marulov.weathertracker.dto.WeatherDto;
 import com.marulov.weathertracker.dto.WeatherResponse;
 import com.marulov.weathertracker.model.Weather;
 import com.marulov.weathertracker.repository.WeatherRepository;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +24,10 @@ import java.util.Optional;
 import static com.marulov.weathertracker.config.WeatherStackConfigProperties.ACCESS_KEY;
 
 @Service
+@CacheConfig(cacheNames = {"weathers"})
 public class WeatherService {
+
+    private static final Logger log = LoggerFactory.getLogger(WeatherService.class);
 
     private final WeatherRepository weatherRepository;
     private final RestTemplate restTemplate;
@@ -28,7 +38,10 @@ public class WeatherService {
         this.restTemplate = restTemplate;
     }
 
+    @Cacheable(key = "#city")
     public WeatherDto getWeatherByCity(String city) {
+
+        log.info("Getting weather for city: {}", city);
         Optional<Weather> weatherOptional = weatherRepository.findFirstByRequestedCityOrderByUpdatedTimeDesc(city);
 
         return weatherOptional.map(weather -> {
@@ -65,5 +78,12 @@ public class WeatherService {
 
     private String getWeatherStackApiUrl(String city) {
         return String.format("http://api.weatherstack.com/current?access_key=%s&query=%s", ACCESS_KEY, city);
+    }
+
+    @CacheEvict(allEntries = true)
+    @PostConstruct
+    @Scheduled(fixedRateString = "10000")
+    public void clearCache() {
+        log.info("Clearing cache");
     }
 }
